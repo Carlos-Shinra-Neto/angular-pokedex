e8889f82e80a17b6a4ca898460744eda74173d30
Carlos-Shinra-Neto <leosergi09@gmail.com>
2022-07-28 20:31:57 -0300

    Projeto Inicial Pokedex-Angular

---
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-card',
+  templateUrl: './pokemon-card.component.html',
+  styleUrls: ['./pokemon-card.component.css']
+})
+export class PokemonCardComponent {
+
+  @Input()
+  pokemon!:string;
+
+  @Input()
+  id!: number;
+
+  pegarImagemPokemon(){
+    const numeroFormatado = this.zerosAntes(this.id);
+    console.log(this.id)
+
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${numeroFormatado}.png`
+  }
+
+  zerosAntes(str: string | number, size = 3): string{
+    let s = String(str);
+    while (s.length<(size || 2)){
+      s = '0' + s;
+    }
+    return s;
+  }
+}
---
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.css']
+})
+export class PokemonListComponent {
+
+  pokemons = ['Gastly', 'Haunter' ,'Gengar']
+  
+}
